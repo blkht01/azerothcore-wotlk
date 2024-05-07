81ca94e31b24f2addf2f51139eb1881da27adf16
Alex <alexvd19@gmail.com>
2024-05-07 14:31:48 +0200

    Creature, Item and Attack Power Scaling changes.
    Multiple items have been changed in Gnomeregan and Graveyard.
    Grubbis Pads have been added to Grubbis loot table in Gnomeregan.
    The Defiant Ring item have been added to the loot table in The Stockade.
    Attack power should now scale with Mortal Strike and Bloodthirst.

---
+++ alex_sql/5_7_2024/Spell_bonus_data_bloodthirst_mortal_strike_scaling.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_bonus_data` WHERE `entry`=12294;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (12294, 1, 0, 0, 0, 'Warrior - Mortal Strike');
+DELETE FROM `spell_bonus_data` WHERE `entry`=23881;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (23881, 1, 0, 0, 0, 'Warrior - Bloodthirst');
