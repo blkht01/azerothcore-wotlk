4d805a33088712702f2a9fc2948e2949c968b7ea
Alex <alexvd19@gmail.com>
2024-05-07 14:39:23 +0200

    Heroic Strike AP Scaling
    Heroic Strike should now scale with attack power.

---
+++ alex_sql/5_7_2024/Heroic_Strike_AP_Scaling.sql
@@ -0,0 +1,8 @@
+DELETE FROM `spell_bonus_data` WHERE `entry`=1608;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (1608, 1, 0, 0, 0, 'Warrior - Heroic Strike (Rank 4)');
+DELETE FROM `spell_bonus_data` WHERE `entry`=285;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (285, 1, 0, 0, 0, 'Warrior - Heroic Strike (Rank 3)');
+DELETE FROM `spell_bonus_data` WHERE `entry`=284;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (284, 1, 0, 0, 0, 'Warrior - Heroic Strike (Rank 2)');
+DELETE FROM `spell_bonus_data` WHERE `entry`=78;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (78, 1, 0, 0, 0, 'Warrior - Heroic Strike (Rank 1)');
