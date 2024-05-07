962d591ff335063a056d5ee99c3796ad7bbe54e4
Antonio Cvitkovic <125733906+ancvitko@users.noreply.github.com>
2024-05-07 02:04:23 +0200

    Added higher rank spells to trainers for casters

---
+++ Ire_sql/5_6_2024/Paladin_Trainer_Spells.sql
@@ -0,0 +1,8 @@
+DELETE FROM `npc_trainer` WHERE `ID`=200004 AND `SpellID`=5614;
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES (200004, 5614, 3600, 0, 0, 25, 0);
+DELETE FROM `npc_trainer` WHERE `ID`=200004 AND `SpellID`=2800;
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES (200004, 2800, 3600, 0, 0, 25, 0);
+DELETE FROM `npc_trainer` WHERE `ID`=200004 AND `SpellID`=20165;
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES (200004, 20165, 3600, 0, 0, 25, 0);
+DELETE FROM `npc_trainer` WHERE `ID`=200004 AND `SpellID`=10291;
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES (200004, 10291, 3600, 0, 0, 25, 0);
