d28f5ca7ec0363bde1f8fcccbbda6d89367c28f5
Antonio Cvitkovic <125733906+ancvitko@users.noreply.github.com>
2024-05-14 18:41:49 +0200

    Added healthstone rank 3 to trainers

---
+++ Ire_sql/5_14_2024/Warlock_Trainer_Add_Healthstone_rank3.sql
@@ -0,0 +1,2 @@
+DELETE FROM `npc_trainer` WHERE `ID`=200010 AND `SpellID`=5699;
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES (200010, 5699, 3600, 0, 0, 25, 0);
