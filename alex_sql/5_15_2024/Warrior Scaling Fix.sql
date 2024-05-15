75621427cebe0b98ff394112a4978a44de33bbce
Alex <alexvd19@gmail.com>
2024-05-15 14:41:32 +0200

    Warrior Scaling Fix
    Removed scaling from Heroic Strike.
    Removed scaling from Execute since its being handled in the core.

---
+++ alex_sql/5_15_2024/Warrior Scaling Fix.sql
@@ -0,0 +1,16 @@
+DELETE FROM `spell_bonus_data` WHERE `entry`=23881;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (23881, 0, 0, 0.202, 0, 'Warrior - Bloodthirst');
+DELETE FROM `spell_bonus_data` WHERE `entry`=845;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (845, 0, 0, 0.156, 0, 'Warrior - Cleave (Rank 1)');
+DELETE FROM `spell_bonus_data` WHERE `entry`=5308;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (5308, 0, 0, 0, 0, 'Warrior - Execute (Rank 1)');
+DELETE FROM `spell_bonus_data` WHERE `entry`=78;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (78, 0, 0, 0, 0, 'Warrior - Heroic Strike (Rank 1)');
+DELETE FROM `spell_bonus_data` WHERE `entry`=284;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (284, 0, 0, 0, 0, 'Warrior - Heroic Strike (Rank 2)');
+DELETE FROM `spell_bonus_data` WHERE `entry`=285;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (285, 0, 0, 0, 0, 'Warrior - Heroic Strike (Rank 3)');
+DELETE FROM `spell_bonus_data` WHERE `entry`=1608;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (1608, 0, 0, 0, 0, 'Warrior - Heroic Strike (Rank 4)');
+DELETE FROM `spell_bonus_data` WHERE `entry`=12294;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (12294, 0, 0, 0.18, 0, 'Warrior - Mortal Strike');
